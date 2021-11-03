f4b22c022a6f2e860a1b55c6b5dabf67270e19ad
danielpietka <77329251+danielpietka@users.noreply.github.com>
2021-11-03 17:53:45 +0100

    lab3

---
+++ Model/Miesiace.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Model;
+
+class Miesiace{
+    public function pobierzWszystkie(){
+        $miesiace = array( "Styczen" => 'blue',
+            "Luty"=> 'yellow',
+            "Marzen"=> 'white',
+            "Kwiecien"=> 'black',
+            "Maj"=> 'red',
+            "Czerwiec"=> 'green',
+            "Lipiec"=> 'orange',
+            "Sierpien"=> 'pink',
+            "Wrzesien"=> 'violet',
+            "Pazdziernik"=> 'purple',
+            "Listopad"=> 'brown',
+            "Grudzien"=> 'grey');
+    
+            return ["Styczen" => 'blue',
+            "Luty"=> 'yellow',
+            "Marzen"=> 'white',
+            "Kwiecien"=> 'black',
+            "Maj"=> 'red',
+            "Czerwiec"=> 'green',
+            "Lipiec"=> 'orange',
+            "Sierpien"=> 'pink',
+            "Wrzesien"=> 'violet',
+            "Pazdziernik"=> 'purple',
+            "Listopad"=> 'brown',
+            "Grudzien"=> 'grey'];
+        }
+
+    
+
+   
+}
